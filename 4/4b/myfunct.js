273bd203c681311db1c5e3f528438cc352c1dc3e
abdul rohman <abdulrohmanunida@gmail.com>
2024-09-26 09:40:22 +0700

    Update the UI

---
+++ 4/4b/myfunct.js
@@ -0,0 +1,24 @@
+function showLoginPage() {
+}
+
+function showRegisterPage() {
+}
+
+function showProvinsiList() {
+}
+
+function showProvinsiDetail(id) {
+}
+
+function showProvinsiForm(id = null) {
+}
+
+function submitProvinsiForm() {
+}
+
+function deleteProvinsi(id) {
+}
+
+$(document).ready(function() {
+    showLoginPage(); 
+});
